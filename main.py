3e9de9282406224b64d557fd4e824e637ef04828
frankhye-cmd <frankhye@udel.edu>
2025-12-06 15:57:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from bakery import assert_equal
 from drafter import *
 from dataclasses import dataclass
 
-from meta import *
-
 hide_debug_information()
 set_website_framed(False)
 set_website_title("Your Drafter Website")
