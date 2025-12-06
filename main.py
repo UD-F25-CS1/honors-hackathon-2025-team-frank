2c849b31c23e1ea1c3fdfaeabd678d44f2211578
frankhye-cmd <frankhye@udel.edu>
2025-12-06 15:55:07 -0500

    Removed none

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ set_site_information(
     """Welcome to the HACK INC login page. The
     goal is pretty simple. Log in to your account by typing in
     the password. """,
-    ["None"],
-    ["None"],
-    ["None"]
 )
 set_website_style("XP")
 
